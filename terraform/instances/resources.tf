f35d0a84fe874444f290e573ed301ea63d8c61bd
DenysMorgun <denys.morhun@gmail.com>
2020-01-07 18:38:18 +0200

    no RDM

--- terraform/instances/resources.tf
+++ terraform/instances/resources.tf
@@ -1,8 +1,9 @@
-resource "aws_instance" "master" {
+resource "aws_instance" "node" {
   ami             = "${data.aws_ami.ubuntu.id}"
   instance_type   = "t2.micro"
+  # count           = 3
 
   tags = {
-    Name = "master"
+    Name = "node"
   }
 }
